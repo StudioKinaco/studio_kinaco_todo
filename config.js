33b34ac537764f8bd8bfe833b39ae77392c29632
StudioKinaco <114233965+StudioKinaco@users.noreply.github.com>
2025-12-12 01:44:30 +0900

    Add configuration for Supabase

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://zmhgslnuzjorxbwdriuz.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InptaGdzbG51empvcnhid2RyaXV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU0NjYxNzAsImV4cCI6MjA4MTA0MjE3MH0.TM5hJyBLWt6fFHQZ8rG_-_onROGdN5bH5qdmDgxREpg"
+};
